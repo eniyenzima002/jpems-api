25933b6fd9866aa465a8014422cbcfc1f231c711
Your Name <you@example.com>
2025-03-13 18:20:49 -0500

    URL update

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ import dashboardRouter from "./routes/dashboard.js"
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://jp-maendeleo-farm.vercel.app/",
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public/uploads"));
